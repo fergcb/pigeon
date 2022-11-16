b2c9d66baab381aab910e7a888d8bf09cd5af4f7
fergcb <fergus.bentley@gmail.com>
2022-11-16 09:30:01 +0000

    Tidy up

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,7 +1,2 @@
 import functions.definitions
 from functions.function_registry import FunctionRegistry as registry
-
-if __name__ == "__main__":
-    for g in registry._groups.values():
-        for f in g._functions:
-            print(f.symbol, f.name, f.params)
